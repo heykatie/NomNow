e624a39da94879e356a5ec181d697c115d28744d
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-02-11 15:41:08 -0800

    prepopulate checkout page with order details when reordering - Files: instance/dev.db react-vite/dist/assets/index-470a6477.js react-vite/dist/index.html react-vite/src/components/Checkout/Checkout.css react-vite/src/components/Checkout/Checkout.jsx react-vite/src/components/Orders/Orders.jsx

--- react-vite/src/components/Checkout/Checkout.jsx
+++ react-vite/src/components/Checkout/Checkout.jsx
@@ -1,5 +1,60 @@
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Checkout';
+
 export default function Checkout() {
-  return (
-    <div>checkout page</div>
-  )
+  const location = useLocation();
+  const navigate = useNavigate();
+	const [orderDetails, setOrderDetails] = useState(
+		location.state?.order || null
+	);
+
+	// useEffect(() => {
+	// 	if (!orderDetails) {
+	// 		navigate('/home');
+	// 	}
+  // }, [orderDetails]);
+
+  useEffect(() => {
+		if (location.state?.order) {
+			setOrderDetails(location.state.order);
+		} else {
+			navigate('/home');
+		}
+  }, [location.state]);
+
+	if (!orderDetails) return null;
+
+	return (
+		<div className='checkout-container'>
+			<h2>Checkout</h2>
+			<p>
+				Reordering from: <strong>{orderDetails.restaurant?.name}</strong>
+			</p>
+			<p>
+				Total: $
+				{orderDetails?.totalCost
+					? orderDetails.totalCost.toFixed(2)
+					: '0.00'}
+			</p>
+
+			<div className='checkout-items'>
+				{orderDetails?.orderItems?.length ? (
+					orderDetails.orderItems.map((item) => (
+						<div key={item.id} className='checkout-item'>
+							<p>
+								{item.name} - {item.quantity}x
+							</p>
+							<p>${item.price.toFixed(2)}</p>
+						</div>
+					))
+				) : (
+					<p>No items found.</p>
+				)}
+			</div>
+
+			<button className='confirm-order-btn'>Confirm Order</button>
+		</div>
+	);
 }
