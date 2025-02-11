66405291cac9fa72119ef13e491ac0b8b101aca1
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-02-11 15:19:28 -0800

    add checkout page - Files: react-vite/dist/assets/index-15792182.js react-vite/dist/index.html react-vite/src/components/Checkout/Checkout.jsx react-vite/src/components/Checkout/index.js react-vite/src/router/index.jsx

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -14,90 +14,94 @@ import UpdateMenuItem from '../components/MenuItemUpdate/MenuItemUpdate';
 import DeleteMenuItem from '../components/MenuItemDelete';
 import AddRestaurant from '../components/CreateRestaurant/AddRestaurant';
 import ManageRestaurants from '../components/ManageRestaurants/ManageRestaurants';
+import Checkout from '../components/Checkout';
 
 import {AccountFormPage, NameFormPage, PhoneNumberPage, EmailPage} from '../components/AccountFormPage'
 
 export const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <LandingPage />,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage isLogin={true} />,
-      },
-      {
-        path: "signup",
-        element: <LoginFormPage isSignup={true} />,
-      },
-      {
-        path: "account",
-        element: <AccountFormPage />,
-      },
-      {
-        path: "account/name",
-        element: <NameFormPage />
-      },
-      {
-        path: "account/phone",
-        element: <PhoneNumberPage />
-      },
-      {
-        path: "account/email",
-        element: <EmailPage />
-      },
-      {
-        path: "wallet",
-        element: <WalletPage />,
-      },
-      {
-        path: "orders",
-        element: <Orders />,
-      },
-      {
-        path: "reviews/:restaurantId",  
-        element: <Reviews />,  
-      },
-      {
-        path: "*",
-        element: <h1>404 Page Not Available</h1>,
-      }, {
-        path: "menu-items",
-        element: <MenuItemList />,
-      },
-      {
-        path: "menu-items/:id",
-        element: < MenuItemDetail/>,
-      },
-      {
-        path: "menu-items/new",
-        element: < MenuItemCreate/>,
-      },
-      {
-        path: "menu-items/:id/update",
-        element: < UpdateMenuItem/>,
-      },
-      {
-        path: "menu-items/:id/delete",
-        element: < DeleteMenuItem/>,
-      },
-      {
-        path: "restaurants/new",  //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login. 
-        element: <AddRestaurant />
-      },
-      {
-        path: "restaurants/manage",  //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login. 
-        element: <ManageRestaurants />
-      },
-
-      {
-        path: "*",
-        element: <h1>404 Page Not Available</h1>,
-      },
-
-    ],
-  },
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <LandingPage />,
+			},
+			{
+				path: 'login',
+				element: <LoginFormPage isLogin={true} />,
+			},
+			{
+				path: 'signup',
+				element: <LoginFormPage isSignup={true} />,
+			},
+			{
+				path: 'account',
+				element: <AccountFormPage />,
+			},
+			{
+				path: 'account/name',
+				element: <NameFormPage />,
+			},
+			{
+				path: 'account/phone',
+				element: <PhoneNumberPage />,
+			},
+			{
+				path: 'account/email',
+				element: <EmailPage />,
+			},
+			{
+				path: 'wallet',
+				element: <WalletPage />,
+			},
+			{
+				path: 'orders',
+				element: <Orders />,
+			},
+			{
+				path: 'reviews/:restaurantId',
+				element: <Reviews />,
+			},
+			{
+				path: '*',
+				element: <h1>404 Page Not Available</h1>,
+			},
+			{
+				path: 'menu-items',
+				element: <MenuItemList />,
+			},
+			{
+				path: 'menu-items/:id',
+				element: <MenuItemDetail />,
+			},
+			{
+				path: 'menu-items/new',
+				element: <MenuItemCreate />,
+			},
+			{
+				path: 'menu-items/:id/update',
+				element: <UpdateMenuItem />,
+			},
+			{
+				path: 'menu-items/:id/delete',
+				element: <DeleteMenuItem />,
+			},
+			{
+				path: 'restaurants/new', //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login.
+				element: <AddRestaurant />,
+			},
+			{
+				path: 'restaurants/manage', //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login.
+				element: <ManageRestaurants />,
+			},
+			{
+				path: 'checkout',
+				element: <Checkout />,
+			},
+			{
+				path: '*',
+				element: <h1>404 Page Not Available</h1>,
+			},
+		],
+	},
 ]);
