73c01bf18442c855fc60f4fa58ce72acd974675d
SamaAlt <althiabatsamai@gmail.com>
2025-02-11 19:01:13 -0500

    Resolved merge conflict in router configuration

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -7,168 +7,94 @@ import WalletPage from '../components/WalletPage/WalletPage';
 import Orders from '../components/Orders';
 import MenuItemList from '../components/MenuItemList/MenuItemList';
 import MenuItemDetail from '../components/MenuItemDetail/MenuItemDetail';
-// import MenuItemForm from '../components/MenuItemForm/MenuItemForm';
 import MenuItemCreate from '../components/MenuItemCreate';
 import UpdateMenuItem from '../components/MenuItemUpdate/MenuItemUpdate';
-// import MenuItem from '../components/MenuItem/MenuItem';
 import DeleteMenuItem from '../components/MenuItemDelete';
 import ManageRestaurants from '../components/ManageRestaurants/ManageRestaurants';
 import Checkout from '../components/Checkout';
-
-import {AccountFormPage, NameFormPage, PhoneNumberPage, EmailPage} from '../components/AccountFormPage'
+import { AccountFormPage, NameFormPage, PhoneNumberPage, EmailPage } from '../components/AccountFormPage';
 import CreateRestaurant from '../components/CreateRestaurant/CreateRestaurant';
 
 export const router = createBrowserRouter([
-<<<<<<< HEAD
-	{
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <LandingPage />,
-			},
-			{
-				path: 'login',
-				element: <LoginFormPage isLogin={true} />,
-			},
-			{
-				path: 'signup',
-				element: <LoginFormPage isSignup={true} />,
-			},
-			{
-				path: 'account',
-				element: <AccountFormPage />,
-			},
-			{
-				path: 'account/name',
-				element: <NameFormPage />,
-			},
-			{
-				path: 'account/phone',
-				element: <PhoneNumberPage />,
-			},
-			{
-				path: 'account/email',
-				element: <EmailPage />,
-			},
-			{
-				path: 'wallet',
-				element: <WalletPage />,
-			},
-			{
-				path: 'orders',
-				element: <Orders />,
-			},
-			{
-				path: 'reviews/:restaurantId',
-				element: <Reviews />,
-			},
-			{
-				path: '*',
-				element: <h1>404 Page Not Available</h1>,
-			},
-			{
-				path: 'menu-items',
-				element: <MenuItemList />,
-			},
-			{
-				path: 'menu-items/:id',
-				element: <MenuItemDetail />,
-			},
-			{
-				path: 'menu-items/new',
-				element: <MenuItemCreate />,
-			},
-			{
-				path: 'menu-items/:id/update',
-				element: <UpdateMenuItem />,
-			},
-			{
-				path: 'menu-items/:id/delete',
-				element: <DeleteMenuItem />,
-			},
-			{
-				path: 'restaurants/new', //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login.
-				element: <CreateRestaurant />,
-			},
-			{
-				path: 'restaurants/manage', //If Logged in Navigates to Add Restaurant Form. If not, to Signup/Login.
-				element: <ManageRestaurants />,
-			},
-			{
-				path: 'checkout',
-				element: <Checkout />,
-			}
-		],
-	},
-=======
   {
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <LandingPage />,
       },
       {
-        path: "login",
+        path: 'login',
         element: <LoginFormPage isLogin={true} />,
       },
       {
-        path: "signup",
+        path: 'signup',
         element: <LoginFormPage isSignup={true} />,
       },
       {
-        path: "account",
+        path: 'account',
         element: <AccountFormPage />,
       },
       {
-        path: "account/name",
-        element: <NameFormPage />
+        path: 'account/name',
+        element: <NameFormPage />,
       },
       {
-        path: "account/phone",
-        element: <PhoneNumberPage />
+        path: 'account/phone',
+        element: <PhoneNumberPage />,
       },
       {
-        path: "account/email",
-        element: <EmailPage />
+        path: 'account/email',
+        element: <EmailPage />,
       },
       {
-        path: "wallet",
+        path: 'wallet',
         element: <WalletPage />,
       },
       {
-        path: "orders",
+        path: 'orders',
         element: <Orders />,
       },
       {
-        path: "reviews/restaurant/:restaurantId", 
+        path: 'reviews/restaurant/:restaurantId', // Updated path from 627f183
         element: <Reviews />,
       },
       {
-        path: "*",
+        path: '*',
         element: <h1>404 Page Not Available</h1>,
-      }, {
-        path: "menu-items",
+      },
+      {
+        path: 'menu-items',
         element: <MenuItemList />,
       },
       {
-        path: "menu-items/:id",
-        element: < MenuItemDetail/>,
+        path: 'menu-items/:id',
+        element: <MenuItemDetail />,
       },
       {
-        path: "menu-items/new",
-        element: < MenuItemCreate/>,
+        path: 'menu-items/new',
+        element: <MenuItemCreate />,
       },
       {
-        path: "menu-items/:id/update",
-        element: < UpdateMenuItem/>,
+        path: 'menu-items/:id/update',
+        element: <UpdateMenuItem />,
       },
       {
-        path: "menu-items/:id/delete",
-        element: < DeleteMenuItem/>,
+        path: 'menu-items/:id/delete',
+        element: <DeleteMenuItem />,
+      },
+      {
+        path: 'restaurants/new', // Added from HEAD
+        element: <CreateRestaurant />,
+      },
+      {
+        path: 'restaurants/manage', // Added from HEAD
+        element: <ManageRestaurants />,
+      },
+      {
+        path: 'checkout', // Added from HEAD
+        element: <Checkout />,
       },
     ],
   },
->>>>>>> 627f183 (reviews)
 ]);
