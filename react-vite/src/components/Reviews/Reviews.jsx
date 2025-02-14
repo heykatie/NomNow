de2d5de92eb1072881625af4904b7d231036db3d
SamaAlt <althiabatsamai@gmail.com>
2025-02-13 20:45:49 -0500

    Restored Reviews components from commit db2e6c5

---
+++ react-vite/src/components/Reviews/Reviews.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useLocation } from 'react-router-dom';
+import { getReviewsForRestThunk } from '../../redux/reviews';
+import ReviewList from './ReviewList';
+import ReviewForm from './ReviewForm';
+import './Reviews.css';
+
+const Reviews = () => {
+  const { restaurantId } = useParams();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const orderId = queryParams.get('orderId');
+  const initialRestaurantName = queryParams.get('restaurantName');
+
+  const dispatch = useDispatch();
+  const reviews = useSelector((state) => state.reviews.allReviewsForRest);
+
+  const [restaurantName, setRestaurantName] = useState(initialRestaurantName);
+
+  useEffect(() => {
+    if (!restaurantName && restaurantId) {
+      fetch(`/api/restaurants/${restaurantId}`)
+        .then(response => response.json())
+        .then(data => setRestaurantName(data.restaurant.name)) 
+        .catch(error => console.error('Error fetching restaurant name:', error));
+    }
+  }, [restaurantId, restaurantName]);
+
+  useEffect(() => {
+    if (restaurantId) {
+      dispatch(getReviewsForRestThunk(restaurantId));
+    }
+  }, [dispatch, restaurantId]);
+
+  return (
+    <div className="reviews-container">
+      <h2>Reviews for {restaurantName || `Restaurant ${restaurantId}`}</h2>
+
+      <ReviewForm restaurantId={restaurantId} orderId={orderId || 1} />
+      <ReviewList reviews={reviews} />
+    </div>
+  );
+};
+
+export default Reviews;
