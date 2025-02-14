54de25f01560ab9f43f5429307ba6961f6395ebe
burak <burakoncuy@gmail.com>
2025-02-13 23:08:32 -0500

    done footers

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -1,102 +1,88 @@
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import './Footer.css';
 
 export default function Footer() {
 
-  const sessionUser = useSelector(state => state.session.user)
-
   return (
     <>
       <div id="footer">
-        <div id='footer-col-1'>
-          <Link exact to="/home">
+        <div id='footer-logo'>
+          <Link exact to="/signup">
             <div id='footer-logo-text'>
               NomNow
             </div>
           </Link>
         </div>
+
         <div className='footer-right'>
-          <div id='footer-col'>
-            <div className='footer-right-header'>Features</div>
-            <div className='footer-list'>
-              <Link exact to="/">
-                <div>Restaurants</div>
-              </Link>
-              <Link exact to="/menu-items">
-                <div>Menu Items</div>
-              </Link>
-              <Link exact to="/reviews/restaurant/1">
-                <div>Reviews</div>
-              </Link>
-              
-                <Link exact to="/orders">
-                  <div>Orders</div>
-                </Link>
-
-                <Link exact to="/">
-                  <div>LandingPage</div>
-                </Link>
-
+          <div className='footer-contact'>
+            <div className='footer-name'>
+            <a href="https://heykatie.github.io/" target="_blank" rel="noopener noreferrer">
+              <div>Gabriel Dean</div>
+              </a>
+              <div className='footer-links'>
+                <a href="https://www.linkedin.com/in/gabriel-dean" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/gabrdean" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
             </div>
-          </div>
-          <div id='footer-col'>
-            <div className='footer-right-header'>Contact Us</div>
-            <div className='footer-list'>
-              <div className='footer-name'>
-                  <div>Gabriel Dean</div>
-                <div className='footer-links'>
-                  <a href="" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                  </a>
-                  <a href="https://github.com/gabrdean" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
+
+            <div className='footer-name'>
+            <a href="https://heykatie.github.io/" target="_blank" rel="noopener noreferrer">
+              <div>Burak Ozdemir</div>
+              </a>
+              <div className='footer-links'>
+                <a href="https://www.linkedin.com/in/burak-%C3%B6zdemir-64a2a9317/" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/burakoncuy" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
-              <div className='footer-name'>
-                  <div>Burak Ozdemir</div>
-                <div className='footer-links'>
-                  <a href="https://www.linkedin.com/in/burak-%C3%B6zdemir-64a2a9317/" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                  </a>
-                  <a href="https://github.com/burakoncuy" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
+            </div>
+
+            <div className='footer-name'>
+            <a href="https://heykatie.github.io/" target="_blank" rel="noopener noreferrer">
+              <div>Samai Althiabat</div>
+              </a>
+              <div className='footer-links'>
+                <a href="https://www.linkedin.com/in/samai-althiabat" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/SamaAlt" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
-              <div className='footer-name'>
-                  <div>Samai Althiabat</div>
-                <div className='footer-links'>
-                  <a href="a" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                  </a>
-                  <a href="https://github.com/SamaAlt" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
+            </div>
+
+            <div className='footer-name'>
+            <a href="https://heykatie.github.io/" target="_blank" rel="noopener noreferrer">
+              <div>Katie Leong</div>
+              </a>
+              <div className='footer-links'>
+                <a href="https://www.linkedin.com/in/katieleonght/" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/heykatie" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
-              <div className='footer-name'>
-                <div>Katie Leong</div>
-                <div className='footer-links'>
-                  <a href="" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                  </a>
-                  <a href="https://github.com/heykatie" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
-              </div>
-              <div className='footer-name'>
-                <div>Marcelle Armstrong</div>
-                <div className='footer-links'>
-                  <a href="" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                  </a>
-                  <a href="https://github.com/Mcode4" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
+            </div>
+
+            <div className='footer-name'>
+            <a href="https://mcode4.github.io/" target="_blank" rel="noopener noreferrer">
+              <div>Marcelle Armstrong</div>
+              </a>
+              <div className='footer-links'>
+                <a href="https://www.linkedin.com/in/marcellies-armstrong/" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/Mcode4" target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
             </div>
           </div>
@@ -107,4 +93,4 @@ export default function Footer() {
       </div>
     </>
   )
-};
+}
